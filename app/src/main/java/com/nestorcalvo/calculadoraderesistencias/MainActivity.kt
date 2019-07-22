2f25004989eeab2f1ff8cb30867fa12bca786cf1
nestorcalvo <nestor.calvo@udea.edu.co>
2019-07-21 20:38:05 -0500

    Initial commit

---
+++ app/src/main/java/com/nestorcalvo/calculadoraderesistencias/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.nestorcalvo.calculadoraderesistencias
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var band1 = 1
+    var band2 = 0
+    var multiplier:Double= 1.0
+    var tolerancy = 0.0
+    var resultado:Double = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun actualizar(view: View) {
+        if (view is Button) {
+
+            when (view.id) {
+                R.id.btblack_2band->
+                    band2 = 0
+                R.id.btblack_3band->
+                    multiplier = 0.0
+                R.id.btbrown_1band->
+                    band1 = 1
+                R.id.btbrown_2band->
+                    band2 = 1
+                R.id.btbrown_3band->
+                    multiplier = 10.0
+                R.id.btbrown_tolband->
+                    tolerancy = 1.0
+
+                R.id.btred_1band->
+                    band1 = 2
+                R.id.btred_2band->
+                    band2 = 2
+                R.id.btred_3band->
+                    multiplier = 100.0
+                R.id.btred_tolband->
+                    tolerancy = 2.0
+
+                R.id.btorange_1band->
+                    band1 = 3
+                R.id.btorange_2band->
+                    band2 = 3
+                R.id.btorange_3band->
+                    multiplier = 1000.0
+
+                R.id.btyellow_1band->
+                    band1 = 4
+                R.id.btyellow_2band->
+                    band2 = 4
+                R.id.btyellow_3band->
+                    multiplier = 10000.0
+
+                R.id.btgreen_1band->
+                    band1 = 5
+                R.id.btgreen_2band->
+                    band2 = 5
+                R.id.btgreen_3band->
+                    multiplier = 100000.0
+                R.id.btgreen_tolband->
+                    tolerancy = 0.5
+
+                R.id.btblue_1band->
+                    band1 = 6
+                R.id.btblue_2band->
+                    band2 = 6
+                R.id.btblue_3band->
+                    multiplier = 1000000.0
+
+
+                R.id.btpurple_1band->
+                    band1 = 7
+                R.id.btpurple_2band->
+                    band2 = 7
+                R.id.btpurple_3band->
+                    multiplier = 10000000.0
+
+
+                R.id.btgray_1band->
+                    band1 = 8
+                R.id.btgray_2band->
+                    band2 = 8
+                R.id.btgray_3band->
+                    multiplier = 100000000.0
+
+                R.id.btwhite_1band->
+                    band1 = 9
+                R.id.btwhite_2band->
+                    band2 = 9
+                R.id.btwhite_3band->
+                    multiplier = 1E8
+
+                R.id.btgold_3band->
+                    multiplier = 0.1
+                R.id.btgold_tolband->
+                    tolerancy = 5.0
+
+                R.id.btsilver_3band->
+                    multiplier = 0.01
+                R.id.btsilver_tolband->
+                    tolerancy = 10.0
+
+
+
+
+
+            }
+        }
+        resultado = ((band1 *10 + band2)*multiplier)
+        tvResultado.text = ("$resultado Ohms ±$tolerancy%")
+
+    }
+}
